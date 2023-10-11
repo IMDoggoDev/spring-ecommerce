85c0469944f7f2b3370e4308f354d2f8f380fe97
Daiyerson <dvprivatejob@gmail.com>
2023-10-11 02:23:48 -0400

    Creación de la estructura del proyecto y las clases model

---
+++ src/main/java/com/doggo/ecommerce/EcommerceApplication.java
@@ -0,0 +1,16 @@
+package com.doggo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class EcommerceApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+		System.out.println("Application Spring");
+	}
+
+}
